2d94bb7872eec33b4014e0f2d6b01112d37a5de6
souhir-Naassaoui <souhir.naassaoui@gmail.com>
2026-07-11 10:36:23 -0400

    update vm ssh

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -135,7 +135,7 @@ resource "azurerm_linux_virtual_machine" "worker" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file(pathexpand("~/.ssh/id_rsa.pub"))
+    public_key = var.ssh_public_key
   }
 
   os_disk {
