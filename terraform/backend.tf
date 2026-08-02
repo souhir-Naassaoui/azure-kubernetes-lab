456c3068bd1369095cc56d3670d95e58586dba8c
souhir-Naassaoui <souhir.naassaoui@gmail.com>
2026-08-02 16:56:39 -0400

    add tfstate backend - az storage

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstatek8slab"
+    storage_account_name = "tfstatek8slab2"
     container_name       = "tfstate"
     key                  = "k8s.tfstate"
   }
