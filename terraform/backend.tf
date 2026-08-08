eba5fc00a50d630fcf9f021602a3218ad0b268b8
souhir-Naassaoui <souhir.naassaoui@gmail.com>
2026-08-08 13:24:14 -0400

    update azure secrets

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstatek8slab2"
+    storage_account_name = "tfstatek8slab"
     container_name       = "tfstate"
     key                  = "k8s.tfstate"
   }
